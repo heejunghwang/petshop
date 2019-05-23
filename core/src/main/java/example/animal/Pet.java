4a29d77b5ba8d36353f05b8dd1f8af916a6e369f
jane.hwang <jane.hwang@memebox.com>
2019-05-23 11:13:05 +0900

    cleanup

--- core/src/main/java/example/animal/Pet.java
+++ core/src/main/java/example/animal/Pet.java
@@ -1,14 +1,11 @@
 package example.animal;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table
-@Data
 public class Pet {
     @Id
     private long id;
@@ -18,4 +15,36 @@ public class Pet {
     private Animal type;
 
     private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Animal getType() {
+        return type;
+    }
+
+    public void setType(Animal type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
