bdbca11da84ae157f395db66af4e31aef17747b3
jane.hwang <jane.hwang@memebox.com>
2019-05-23 11:33:21 +0900

    cleanup

--- core/src/main/java/example/animal/PetInput.java
+++ core/src/main/java/example/animal/PetInput.java
@@ -1,36 +1,12 @@
 package example.animal;
 
-/**
- * Created by hwang on 21/05/2019.
- */
+import lombok.Data;
+
+@Data
 public class PetInput {
     private String name;
 
     private Animal type;
 
     private int age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Animal getType() {
-        return type;
-    }
-
-    public void setType(Animal type) {
-        this.type = type;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
--- core/src/main/java/example/service/PetService.java
+++ core/src/main/java/example/service/PetService.java
@@ -12,6 +12,7 @@ import java.util.List;
 @Service
 @Transactional
 public class PetService {
+
     @Autowired
     private PetRepository petRepository;
 
