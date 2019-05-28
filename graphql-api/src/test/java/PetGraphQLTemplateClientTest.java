769237156a44e053d4563cab68c48e520c0a361f
HeeJung Hwang <gyahoo617@gmail.com>
2019-05-29 00:08:34 +0900

    fix unit test

--- graphql-api/src/test/java/PetGraphQLTemplateClientTest.java
+++ graphql-api/src/test/java/PetGraphQLTemplateClientTest.java
@@ -4,19 +4,25 @@ import com.graphql.spring.boot.test.GraphQLResponse;
 import com.graphql.spring.boot.test.GraphQLTestTemplate;
 import example.GraphqlApp;
 import example.domain.Animal;
+import example.domain.Pet;
 import example.domain.PetInput;
 import example.service.PetService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(SpringRunner.class)
@@ -26,7 +32,7 @@ public class PetGraphQLTemplateClientTest {
     @Autowired
     private GraphQLTestTemplate graphQLTestTemplate;
 
-    @Autowired
+    @MockBean
     private PetService petService;
 
     @Test
@@ -34,11 +40,18 @@ public class PetGraphQLTemplateClientTest {
         //given
         ObjectNode petParam = new ObjectMapper().createObjectNode();
         petParam.put("type", Animal.DOG.toString());
-        petParam.put("name", "petName");
+        petParam.put("name", "my_pet");
         petParam.put("age", 10);
 
         ObjectNode queryVariables = new ObjectMapper().createObjectNode();
         queryVariables.replace("pet", petParam);
+
+        Pet pet = new Pet();
+        pet.setId(1);
+        pet.setAge(20);
+        pet.setName("my_pet");
+        pet.setType(Animal.DOG);
+        when(petService.createPet(any(PetInput.class))).thenReturn(pet);
 
         //when
         GraphQLResponse response = graphQLTestTemplate.perform("graphql/create-pet.graphql", queryVariables);
@@ -46,17 +59,20 @@ public class PetGraphQLTemplateClientTest {
         //then
         assertNotNull(response);
         assertNotNull(response.get("$.data.createPet.name"));
-        assertEquals("petName", response.get("$.data.createPet.name"));
+        assertEquals("my_pet", response.get("$.data.createPet.name"));
     }
 
     @Test
     public void testGetPets() throws IOException {
         //given
-        PetInput petInput = new PetInput();
-        petInput.setName("my_pet");
-        petInput.setType(Animal.BADGER);
-        petInput.setAge(20);
-        petService.createPet(petInput);
+        Pet pet = new Pet();
+        pet.setId(1);
+        pet.setAge(20);
+        pet.setName("my_pet");
+        pet.setType(Animal.DOG);
+        List<Pet> petList = new ArrayList<>();
+        petList.add(pet);
+        when(petService.findPets()).thenReturn(petList);
 
         //when
         GraphQLResponse response = graphQLTestTemplate.postForResource("graphql/post-pets.graphql");
